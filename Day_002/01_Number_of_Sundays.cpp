ef16e6e12537d8c58c2046302c90f6a52a563dac
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-02 21:17:16 +0530

    code added

---
+++ Day_002/01_Number_of_Sundays.cpp
@@ -0,0 +1,54 @@
+// Question Link: https://www.interviewbit.com/problems/number-of-sundays/
+
+/*
+Problem Description:
+Given the start day of the month A and number of days in the month B, find number of sundays in the month.
+
+Problem Constraints
+A = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}
+1 <= B <= 109
+
+Input Format:
+First argument is an string A.
+Second argument is an integer B.
+
+Output Format:
+Return an integer.
+
+Example Input
+Input 1:
+A = "Sunday"
+B = 1
+Input 2:
+A = "Monday"
+B = 14
+
+Example Output
+Output 1:
+1
+Output 2:
+2
+
+Example Explanation:
+Explanation 1:
+The only day in the month is sunday.
+Explanation 2:
+
+The 7th and 14th day of the month will be sunday
+*/
+
+
+// Time Complexity = O(1), Space Complexity = O(1)
+int Solution::solve(string A, int B) {
+    unordered_map<string, int> mp;
+   
+    mp["Monday"] = 1;
+    mp["Tuesday"] = 2;
+    mp["Wednesday"] = 3;
+    mp["Thursday"] = 4;
+    mp["Friday"] = 5;
+    mp["Saturday"] = 6;
+    mp["Sunday"] = 7;
+   
+    return (B+mp[A]-1)/7;
+}
