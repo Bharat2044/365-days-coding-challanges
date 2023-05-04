5210a9d86e6973d2410bc853e5431da8600fad2e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-04 17:32:43 +0530

    code added

---
+++ Day_004/01_Product_of_Digits.cpp
@@ -0,0 +1,50 @@
+/*
+Problem Description:
+Given an integer A.
+Find the product of all of it's digits.
+
+Problem Constraints:
+0 <= A <= 109
+
+Input Format:
+Given an integer.
+
+Output Format:
+Return an integer.
+
+Example Input:
+Input 1:
+A = 111
+Input 2:
+A = 123
+
+Example Output:
+Output 1:
+1
+Output 2:
+6
+
+Example Explanation:
+Explanation 1:
+111 -> 1×1×1 = 1
+Explanation 2:
+
+123 -> 1×2×3 = 6
+*/
+
+
+// Time Complexity = O(logA), Space Complexity = O(1)
+int Solution::solve(int A) {
+    if(A == 0)
+        return 0;
+        
+    int ans = 1;
+    
+    while(A != 0) {
+        int d = A%10;        
+        ans *= d;
+        A /= 10;
+    }
+    
+    return ans;
+}
