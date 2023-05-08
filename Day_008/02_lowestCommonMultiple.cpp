aa23a9390c6046687d10b5ecb40c8fbb3f31f3d4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-08 21:12:37 +0530

    code added

---
+++ Day_008/02_lowestCommonMultiple.cpp
@@ -0,0 +1,61 @@
+// Question Link: https://www.interviewbit.com/problems/lowest-common-multiple-lcm/
+
+/*
+Lowest Common Multiple (LCM):
+
+Problem Description:
+Find Lowest Common Multiple of given two integers.
+
+Problem Constraints:
+1 <= A <= 109
+1 <= B <= 109
+
+Input Format:
+First argument is an integer A.
+Second argument is an integer B.
+
+Output Format:
+Return an integer.
+
+Example Input:
+Input 1:
+A = 6
+B = 4
+Input 2:
+A = 1
+B = 11
+
+
+Example Output:
+Output 1:
+12
+Output 2:
+11
+
+Example Explanation:
+Explanation 1:
+12 is the smallest integer which is divisible by 4 and 6 both.
+Explanation 2:
+11 is the smallest integer which is divisible by 1 and 11 both.
+*/
+
+
+
+// Time COmplexity = O(log(min(A, B)), SPace Complexity = O(1)
+long gcd(int x, int y) {
+    
+    while(x != y) {
+        if(x > y)
+            x = x-y;
+        else
+            y = y-x;
+    }
+    
+    return x;
+}
+
+long Solution::solve(int A, int B) {    
+    
+    long ans = (A*B)/gcd(A, B);
+    return  ans;
+}
