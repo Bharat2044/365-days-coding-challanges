544a82f99adfc7ec8c30406c0c9e31b549e4e458
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-05-15 20:07:48 +0530

    code added

---
+++ Day_015/02_factorialTrailingZeroes.cpp
@@ -0,0 +1,39 @@
+// Question Link: https://leetcode.com/problems/factorial-trailing-zeroes/submissions/
+
+/*
+172. Factorial Trailing Zeroes:
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1. 
+
+Example 1:
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+Input: n = 0
+Output: 0 
+
+Constraints:
+0 <= n <= 104
+*/
+
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        
+        while(n) {
+            n /= 5;
+            ans += n;
+        }
+        
+        return ans;
+    }
+};
