9b8c962986798481cd769321637a596517d14bab
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-01 20:49:03 +0530

    code added

---
+++ Day_032/02_validPalindrome.cpp
@@ -0,0 +1,55 @@
+// Question Link: https://leetcode.com/problems/valid-palindrome/
+
+/*
+125. Valid Palindrome:
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+Constraints:
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    int start=0;
+       int end=s.size()-1;
+        
+       while(start<=end){
+           if(!isalnum(s[start])){
+               start++; 
+               continue;
+           }
+           if(!isalnum(s[end])){
+               end--;
+               continue;
+           }
+           if(tolower(s[start])!=tolower(s[end]))
+               return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+        
+       return true;
+    }
+};
