269a3c687d170e91f07ba390d5dd29473b4367e4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-09 22:21:45 +0530

    code added

---
+++ Day_040/02_thirdMaximumNumber.cpp
@@ -0,0 +1,58 @@
+// Question Link: https://leetcode.com/problems/third-maximum-number/
+
+/*
+414. Third Maximum Number
+
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number. 
+
+Example 1:
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+Example 2:
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+ 
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Can you find an O(n) solution?
+*/
+
+
+
+// Time Complexity = O(N), Space Complexity = O(N)
+
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int>s;
+        
+        for(int i=0; i<nums.size(); i++)
+            s.insert(nums[i]);
+        
+        if(s.size() >= 3){   // when set size >=3 means 3rd Maximum exist(because set does not contain duplicate element)
+            int Third_index_from_last = s.size()-3;
+            auto third_maximum = next(s.begin(),Third_index_from_last);
+            return *third_maximum;
+        }
+        return *--s.end(); // return maximum if 3rd maximum not exist
+    }
+};
