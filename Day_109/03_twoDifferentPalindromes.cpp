726348abc9d8ad3fb4ddd8aae46d128e10702e59
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-16 17:16:35 +0530

    code added

---
+++ Day_109/03_twoDifferentPalindromes.cpp
@@ -0,0 +1,25 @@
+// Question Link: https://www.codechef.com/problems/TWODIFFPALIN
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        
+        if((a%2 != 0 && b%2 != 0) || a == 1 || b == 1)
+            cout << "No\n";
+        else
+            cout << "Yes\n";
+    }
+    
+    return 0;
+}
