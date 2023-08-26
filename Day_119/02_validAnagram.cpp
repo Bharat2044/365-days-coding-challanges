0c897255b02a30cf30c939e67d483cac4c4acce3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-26 18:34:43 +0530

    code added

---
+++ Day_119/02_validAnagram.cpp
@@ -0,0 +1,48 @@
+// Question Link: https://leetcode.com/problems/valid-anagram/
+
+/*
+242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false 
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> count;
+        
+        // Count the frequency of characters in string s
+        for (auto x : s) {
+            count[x]++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (auto x : t) {
+            count[x]--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
