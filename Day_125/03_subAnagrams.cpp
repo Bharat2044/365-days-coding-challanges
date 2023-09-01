bce147a63f971c9b51c8aad554dbb437e64db60c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-09-01 17:36:10 +0530

    code added

---
+++ Day_125/03_subAnagrams.cpp
@@ -0,0 +1,36 @@
+// Question Link: https://www.codechef.com/problems/SUBANAGR
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string a;
+    cin >> a;
+    
+    for (int i = 1; i < n; i++) {
+        string b, s = "";
+        cin >> b;
+        
+        for (int p = 0; p < a.size(); p++) {
+            for (int q = 0; q < b.size(); q++) {
+                if (a[p] == b[q]) {
+                    s += b[q];
+                    b.erase(b.begin() + q, b.begin() + q + 1);
+                    break;
+                }
+            }
+        }
+        a = s;
+    }
+    
+    sort(a.begin(), a.end());
+    if (a.length() == 0)
+        cout << "no such string" << endl;
+    else
+        cout << a << endl;
+        
+    return 0;
+}
