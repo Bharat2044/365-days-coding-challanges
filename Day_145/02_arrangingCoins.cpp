264235d3924e59997f7ea7b4be1b131b419daf0d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-09-21 18:14:45 +0530

    code added

---
+++ Day_145/02_arrangingCoins.cpp
@@ -0,0 +1,44 @@
+// Question Link: https://leetcode.com/problems/arranging-coins/
+
+/*
+441. Arranging Coins
+
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+Example 1:
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+ 
+Constraints:
+1 <= n <= 231 - 1
+*/
+
+
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+       long long i=0;
+      long long j=n;
+      
+        while(i<=j)
+        {
+            long long m=(i+j)/2;
+            if((m*(m+1))/2 >n)
+            j=m-1;
+            else
+            {
+                i=m+1;
+              
+            }
+        }
+        return (int)i-1;
+    }
+};
