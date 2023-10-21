d79bdf0f160a73a5d247b2cb5a9206bcea2fe0fc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-21 21:13:33 +0530

    code added

---
+++ Day_175/02_permutationSequence.cpp
@@ -0,0 +1,46 @@
+// Question Link: https://leetcode.com/problems/permutation-sequence/
+
+/*
+60. Permutation Sequence
+
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Example 1:
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+Input: n = 4, k = 9
+Output: "2314"
+Example 3:
+Input: n = 3, k = 1
+Output: "123"
+ 
+Constraints:
+1 <= n <= 9
+1 <= k <= n!
+*/
+
+
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<char> S(n);
+        iota(S.begin(), S.end(), '1');
+        
+        for(int i=1; i<k; i++)
+           next_permutation(S.begin(), S.end()); 
+
+        return string(S.begin(), S.end());
+    }
+};
