eebd7f5e645206f2585ad78630da15c1172dd630
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-09 18:48:37 +0530

    code added

---
+++ Day_194/03_rockPaperScissor.cpp
@@ -0,0 +1,54 @@
+// Question Link: https://www.codechef.com/problems/PASCRO
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string A ;
+        cin >> A ;
+        string B ;
+        cin>> B;
+
+        int counter1 = 0, counter2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            
+            //whenever chefina wins , her points increases so counter2
+            //also increases
+            if ((A[i] == 'R' && B[i] == 'P') ||
+            (A[i] == 'S' && B[i] == 'R') || 
+            (A[i] == 'P' && B[i] == 'S'))
+            {
+                counter2 ++;
+            }
+            //whenever chef wins , his points increases so counter1
+            //also increases
+            else if ((A[i] == 'R' && B[i] == 'S') ||
+            (A[i] == 'P' && B[i] == 'R') ||
+            (A[i] == 'S' && B[i] == 'P'))
+            {
+                counter1 ++;
+            } 
+            
+        }
+
+        int num = 0;
+        if (counter2 >= counter1) {
+            num = ((counter1 + counter2) / 2) + 1;
+            cout << num - counter1 << endl;
+        } 
+        else 
+            cout << 0 << endl;
+        
+    }
+
+    return 0;
+}
