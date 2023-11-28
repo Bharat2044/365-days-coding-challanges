631850b41e33a690a248534542baf28d791d30b2
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-28 18:03:38 +0530

    code added

---
+++ Day_213/02_minimumWindowSubstring.cpp
@@ -0,0 +1,60 @@
+// Question Link: https://leetcode.com/problems/minimum-window-substring/
+
+/*
+76. Minimum Window Substring
+
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+
+Constraints:
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+*/
+
+
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for (char c : t) {
+            map[c]++;
+        }
+
+        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
+        while (end < s.size()){
+            if (map[s[end++]]-- > 0) {
+                counter--;
+            }
+            while (counter == 0) {
+                if (end - begin < d) {
+                    head = begin;
+                    d = end - head;
+                }
+                if (map[s[begin++]]++ == 0) {
+                    counter++;
+                }
+            }  
+        }
+        return d == INT_MAX ? "" : s.substr(head, d);
+    }
+};
