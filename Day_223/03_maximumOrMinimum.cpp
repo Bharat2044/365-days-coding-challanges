8e40d60415b18984c671c92088c8b84e09fb834e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 19:20:10 +0530

    code added

---
+++ Day_223/03_maximumOrMinimum.cpp
@@ -0,0 +1,34 @@
+// Question Link: https://www.codechef.com/problems/MAXORMIN
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int a[n];
+        int one=0,zero=0;
+        
+        for (int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]==0)
+                zero++;
+            else 
+                one++;
+        }
+        
+        if(one>=zero) 
+            cout<<1;
+        else 
+            cout<<0;
+        cout<<endl;
+    }
+    
+	return 0;
+}
