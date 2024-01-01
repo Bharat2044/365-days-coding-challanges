5f9e3792fa8a1035a3720a4df6f5c7a79bad82a9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-01 09:32:08 +0530

    code added

---
+++ Day_247/03_thalaForAReason.cpp
@@ -0,0 +1,18 @@
+// Question Link: https://www.codechef.com/problems/THALA7
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes hereelse 
+	int n;
+	cin>>n;
+	
+	if(n==7)
+	    cout<<"THALA";
+	else
+	    cout<<"SADGE";
+	    
+	return 0;
+}
