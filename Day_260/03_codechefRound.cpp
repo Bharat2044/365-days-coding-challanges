dc5f9f86be861a23db27a0ade2c327c0b98e8c34
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-14 02:36:55 +0530

    code added

---
+++ Day_260/03_codechefRound.cpp
@@ -0,0 +1,17 @@
+// Question Link: https://www.codechef.com/problems/CODECHEF
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	if(t==4)
+	    cout<<"yes\n";
+	else
+	    cout<<"no\n";
+	
+	return 0;
+}
