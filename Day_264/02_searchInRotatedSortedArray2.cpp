647ee64bcb37db29ef0833326b01b01b43f34acd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-18 12:30:23 +0530

    code added

---
+++ Day_264/02_searchInRotatedSortedArray2.cpp
@@ -0,0 +1,71 @@
+// Question Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+/*
+81. Search in Rotated Sorted Array II
+
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+Example 1:
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+ 
+Constraints:
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+*/
+
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] == target)
+                return true;
+			// with duplicates we can have this contdition, just update left & right
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
+            }
+			// first half
+			// first half is in order
+            else if(nums[l] <= nums[mid])
+            {
+				// target is in first  half
+                if((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+			// second half
+			// second half is order
+			// target is in second half
+            else
+            {
+                if((nums[mid] < target) && (nums[r]>= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return false;
+    }
+};
