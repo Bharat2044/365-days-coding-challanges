b4c0ebfde32a25d6f5afec9ea999ccdf9cafacbf
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-28 13:50:11 +0530

    code added

---
+++ Day_274/02_smallestRange1.cpp
@@ -0,0 +1,42 @@
+// Question Link: https://leetcode.com/problems/smallest-range-i/
+
+/*
+908. Smallest Range I
+
+You are given an integer array nums and an integer k.
+In one operation, you can choose any index i where 0 <= i < nums.length and change nums[i] to nums[i] + x where x is an integer from the range [-k, k]. You can apply this operation at most once for each index i.
+The score of nums is the difference between the maximum and minimum elements in nums.
+Return the minimum score of nums after applying the mentioned operation at most once for each index in it.
+
+Example 1:
+Input: nums = [1], k = 0
+Output: 0
+Explanation: The score is max(nums) - min(nums) = 1 - 1 = 0.
+Example 2:
+Input: nums = [0,10], k = 2
+Output: 6
+Explanation: Change nums to be [2, 8]. The score is max(nums) - min(nums) = 8 - 2 = 6.
+Example 3:
+Input: nums = [1,3,6], k = 3
+Output: 0
+Explanation: Change nums to be [4, 4, 4]. The score is max(nums) - min(nums) = 4 - 4 = 0.
+ 
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 104
+0 <= k <= 104
+*/
+
+
+
+class Solution {
+public:
+    int smallestRangeI(vector<int> A, int K) {
+        int mx = A[0], mn = A[0];
+        
+        for (int a : A) 
+            mx = max(mx, a), mn = min(mn, a);
+
+        return max(0, mx - mn - 2 * K);
+    }
+};
