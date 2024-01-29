0d7988b1e7c2bf55faf169df0f82257b29a9fe2d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-29 14:34:49 +0530

    code added

---
+++ Day_275/03_fakeGCD.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://www.codechef.com/problems/FAKEGCD
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    for(int i=0;i<n;i++){
+	        cout<<i+1<<" ";
+	    }
+	    
+	    cout<<endl;
+	}
+	
+	return 0;
+}
