19d90ec3e121038ec23ad9222ed8d04b9e2894d2
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-02-07 14:45:21 +0530

    code added

---
+++ Day_284/03_foodCosts.cpp
@@ -0,0 +1,15 @@
+// Question Link: https://www.codechef.com/problems/FOODCOST
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	
+	int total = 6 * x + 1 * y;
+	cout << total;
+
+	return 0;
+}
