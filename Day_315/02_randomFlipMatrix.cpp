e6345bc62c6b1f8498c3be5e4327038644008cd7
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-09 04:33:28 +0530

    code added

---
+++ Day_315/02_randomFlipMatrix.cpp
@@ -0,0 +1,64 @@
+// Question Link: https://leetcode.com/problems/random-flip-matrix/
+
+/*
+519. Random Flip Matrix
+
+There is an m x n binary grid matrix with all the values set 0 initially. Design an algorithm to randomly pick an index (i, j) where matrix[i][j] == 0 and flips it to 1. All the indices (i, j) where matrix[i][j] == 0 should be equally likely to be returned.
+
+Optimize your algorithm to minimize the number of calls made to the built-in random function of your language and optimize the time and space complexity.
+
+Implement the Solution class:
+Solution(int m, int n) Initializes the object with the size of the binary matrix m and n.
+int[] flip() Returns a random index [i, j] of the matrix where matrix[i][j] == 0 and flips it to 1.
+void reset() Resets all the values of the matrix to be 0.
+ 
+Example 1:
+Input
+["Solution", "flip", "flip", "flip", "reset", "flip"]
+[[3, 1], [], [], [], [], []]
+Output
+[null, [1, 0], [2, 0], [0, 0], null, [2, 0]]
+
+Explanation:
+Solution solution = new Solution(3, 1);
+solution.flip();  // return [1, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+solution.flip();  // return [2, 0], Since [1,0] was returned, [2,0] and [0,0]
+solution.flip();  // return [0, 0], Based on the previously returned indices, only [0,0] can be returned.
+solution.reset(); // All the values are reset to 0 and can be returned.
+solution.flip();  // return [2, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+ 
+Constraints:
+1 <= m, n <= 10^4
+There will be at least one free cell for each call to flip.
+At most 1000 calls will be made to flip and reset.
+*/
+
+
+
+class Solution {
+    int m,n;
+    set<pair<int,int>>s;
+public:
+    Solution(int m, int n) {
+        this -> m = m;
+        this -> n = n;
+        s.clear();
+    }
+    
+    vector<int> flip() {
+        int r = rand()%m;
+        int c = rand()%n;
+
+        while(s.count({r,c}) > 0){
+            r = rand()%m;
+            c = rand()%n;
+        }
+
+        s.insert({r,c});
+        return {r,c};
+    }
+    
+    void reset() {
+        s.clear();
+    }
+};
