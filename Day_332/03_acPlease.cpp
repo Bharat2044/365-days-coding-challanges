9ce8434cdb577c66dcc65ad60ac9a0fd9ec945b4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-26 14:33:55 +0530

    code added

---
+++ Day_332/03_acPlease.cpp
@@ -0,0 +1,21 @@
+// Question Link: https://www.codechef.com/problems/ACPLZ?tab=statement
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+
+    if (t > 30) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+
+}
