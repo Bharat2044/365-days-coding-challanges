58130f95f37d49a90e481362b69f6b2fd55aceb5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-19 06:57:08 +0530

    code added

---
+++ Day_356/03_atmMachine.cpp
@@ -0,0 +1,33 @@
+// Question Link: https://www.codechef.com/problems/ATM2
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+       
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= k) {
+                cout << "1";
+                k -= arr[i];
+            }
+            else
+                cout << "0";
+        }
+    
+        cout << endl;
+    }
+    
+    return 0;
+}
