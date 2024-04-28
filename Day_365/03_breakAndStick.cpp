aa7fdf67d3636adf6ed77c3e136233fe19272351
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-28 17:13:33 +0530

    code added

---
+++ Day_365/03_breakAndStick.cpp
@@ -0,0 +1,26 @@
+// Question Link: https://www.codechef.com/problems/BREAKSTICK
+
+
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll n, x;
+        cin >> n >> x;
+      
+        string ans = "YES";
+
+        if (x % 2 == 0 && (n % 2) != 0)
+            ans = "NO";
+
+        cout << ans << endl;
+    }
+  
+    return 0;
+}
