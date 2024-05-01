17e5cb2ea8230651c8b2c94bbf241734fa996e10
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-01 18:12:59 +0530

    code added

---
+++ Day_368/03_oddPairs.cpp
@@ -0,0 +1,19 @@
+// Question Link: https://www.codechef.com/problems/ODDPAIRS
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        long long n;
+        cin >> n;
+        
+        cout << n * n / 2 << endl;
+    }
+    
+    return 0;
+}
