d6a5158795cb37719f9746dc04fe85d02fff2160
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-06 12:30:05 +0530

    code added

---
+++ Day_373/03_zeroString.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://www.codechef.com/problems/ZEROSTRING
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, z;
+        cin >> n;
+    
+        string sr;
+        cin >> sr;
+    
+        z = count(sr.begin(), sr.end(), '0');
+    
+        cout << ((z < (n - z)) ? (z + 1) : (n - z)) << "\n";
+    }
+    
+    return 0;
+}
