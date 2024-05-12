273da67210e1c37cd6bbd8e21612d4d2681b2b97
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-12 12:51:57 +0530

    code added

---
+++ Day_379/03_luckeySeven.cpp
@@ -0,0 +1,15 @@
+// Question Link: https://www.codechef.com/problems/LUCKYSEVEN
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // your code goes here
+    string S;
+    cin >> S;
+    
+    cout << S[6] << endl;
+
+    return 0;
+}
