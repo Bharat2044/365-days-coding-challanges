63e7b13863da9f6977aec9bcb7f2cb35dbaf1b8f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-18 14:21:13 +0530

    code added

---
+++ Day_385/03_applesAndOranges.cpp
@@ -0,0 +1,22 @@
+// Question Link: https://www.codechef.com/problems/APPLEORANGE
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int N, M, ans;
+        cin >> N >> M;
+
+        ans = __gcd(N, M);
+
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
