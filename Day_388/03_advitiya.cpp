aa498b1975cbf8a53ea8e26aba6ab51e03c197b3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-21 15:59:52 +0530

    code added

---
+++ Day_388/03_advitiya.cpp
@@ -0,0 +1,14 @@
+// Question Link: https://www.codechef.com/problems/ADVITIYA1
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << ((n <= 18 && n >= 16) ? "ADVITIYA" : "WAITING FOR ADVITIYA");
+
+    return 0;
+}
