7d7837e7153744ee886addb94c809c79329dc80e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-05-29 21:05:50 +0530

    code added

---
+++ Day_396/02_threeFriends.cpp
@@ -0,0 +1,24 @@
+// Question Link: https://www.codechef.com/problems/THREEFR
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+    
+        if ((x + y - z == 0) || (x - y + z == 0) || (-x + y + z == 0)) {
+            cout << "yes" << endl;
+            continue;
+        }
+    
+        cout << "no" << endl;
+    }
+    
+    return 0;
+}
